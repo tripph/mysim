9f94887d04afa4834a69d3be194af69272dbef6f
Tripp Hill <thilltbc@gmail.com>
2018-05-30 20:52:54 -0400

    CORS

---
+++ src/main/java/com/tripphill/mysim/Config/CORS.java
@@ -0,0 +1,16 @@
+package com.tripphill.mysim.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORS implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:4200","https://mysim.herokuapp.com");
+    }
+}
+
